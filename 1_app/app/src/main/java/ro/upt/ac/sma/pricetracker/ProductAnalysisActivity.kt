ae20b8a4b6a30b6f237df517ed7fb8dd3c0f7e0b
Luigi Bolovan <luigibolovan@gmail.com>
2020-12-05 00:33:04 +0200

    app startup

---
+++ 1_app/app/src/main/java/ro/upt/ac/sma/pricetracker/ProductAnalysisActivity.kt
@@ -0,0 +1,11 @@
+package ro.upt.ac.sma.pricetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductAnalysisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_analysis)
+    }
+}
